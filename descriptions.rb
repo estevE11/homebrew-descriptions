ed5770f3b687dd4941465115d21bbe5421213d3a
Roger Esteve <rogeresteve30@gmail.com>
2021-02-24 23:36:05 +0100

    Printing desc by names from file

--- descriptions.rb
+++ descriptions.rb
@@ -2,14 +2,20 @@ require 'httparty'
 
 def get_desc(name)
     desc = nil
-    response = HTTParty.get('https://formulae.brew.sh/api/' + name + '.json')
+    response = HTTParty.get('https://formulae.brew.sh/api/formula/' + name + '.json')
     if response.code == 200
-        desc = JSON.parse(response.body)[0]["desc"]
+        desc = JSON.parse(response.body)["desc"]
     else
         return "Not found"
     end
     return desc
 end
 
-desc = get_desc("cask")
-puts desc
+def get_descs(filename)
+    file = File.open(filename)
+    file_data = file.readlines.map(&:chomp)
+    puts file_data
+    File.foreach(filename) {|line| puts line[0..-2] + ': ' + get_desc(line[0..-2])}
+end
+
+get_descs('leaves')
